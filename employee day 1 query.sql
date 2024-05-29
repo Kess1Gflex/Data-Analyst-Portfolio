4f21094f60d2570fd72eeeb71da943a33dc2c8ae
Kess1Gflex <125300224+Kess1Gflex@users.noreply.github.com>
2024-05-29 19:17:43 +0100

    Add files via upload
    I recently began  a 30-day challenge into becoming the best at data analysis. These are the files and query for Day 1

---
+++ employee day 1 query.sql
@@ -0,0 +1,22 @@
+SELECT * FROM [Employee Data]
+
+SELECT Gender, Department FROM [Employee Data]
+
+SELECT Satisfaction, Attrition FROM [Employee Data]
+
+SELECT Department, Salary, EducationField FROM [Employee Data]
+
+SELECT Gender, Department FROM [Employee Data]
+
+SELECT * FROM [Employee Data] WHERE Department ='Sales'
+
+SELECT * FROM [Employee Data] WHERE [Home_Office] = 'Far'
+
+SELECT * FROM [Employee Data] WHERE Satisfaction = 'Low'
+
+SELECT * FROM [Employee Data] WHERE Attrition = 'Yes'
+
+SELECT * FROM [Employee Data] WHERE [Business_Travel] = 'Frequent'
+
+SELECT Gender, Department FROM [Employee Data] ORDER BY Department
+
